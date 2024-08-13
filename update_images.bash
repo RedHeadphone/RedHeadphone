3434179ead1f01640a026a9277eb15ca86f20bcc
RedHeadphone <khilawala92@gmail.com>
2024-08-13 19:01:05 +0530

    use bash instead of python script + update stats

---
+++ update_images.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Directory to save images
+stats_dir="stats"
+mkdir -p "$stats_dir"
+
+# Associative array mapping image file names to API endpoints
+declare -A image_api_endpoints=(
+    ["github_stats.svg"]="https://github-readme-stats.vercel.app/api?username=RedHeadphone&show_icons=true&hide=contribs&theme=github_dark&border_color=404040"
+    ["top_langs.svg"]="https://github-readme-stats.vercel.app/api/top-langs/?username=RedHeadphone&layout=compact&langs_count=6&theme=github_dark&border_color=404040"
+    ["codeforces_stats.svg"]="https://codeforces-readme-stats.vercel.app/api/card?username=redheadphone&theme=github_dark&force_username=true&border_color=404040"
+    ["leetcode_stats.svg"]="https://leetcard.jacoblin.cool/redheadphone?theme=dark&font=noto_sans&ext=contest&sheets=https://gist.githubusercontent.com/RedHeadphone/5e715e284c89cace8f5fa09f7fb930b8/raw/ec0be570f114124b1a2156a660d67baa0ab5639d/leetcode_stats_card.css"
+)
+
+# Function to download an image
+download_image() {
+    local image_name="$1"
+    local url="$2"
+    curl -o "${stats_dir}/${image_name}" -s -w "%{http_code}" "${url}" | {
+        read -r http_code
+        if [ "$http_code" != "200" ]; then
+            echo "Failed to download ${image_name} from ${url}"
+        fi
+    }
+}
+
+# Main loop to download all images
+for image_name in "${!image_api_endpoints[@]}"; do
+    download_image "$image_name" "${image_api_endpoints[$image_name]}"
+done
