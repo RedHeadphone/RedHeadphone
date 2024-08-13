8063a5dceefc4971a2aa217b05b6ca5cd3d67c43
RedHeadphone <khilawala92@gmail.com>
2024-08-13 15:56:59 +0530

    update README.md

--- update_images.py
+++ update_images.py
@@ -9,7 +9,7 @@ os.makedirs(stats_dir, exist_ok=True)
 image_api_endpoints = {
     "github_stats.svg": "https://github-readme-stats.vercel.app/api?username=RedHeadphone&show_icons=true&count_private=true&theme=github_dark&border_color=404040",
     "top_langs.svg": "https://github-readme-stats.vercel.app/api/top-langs/?username=RedHeadphone&layout=compact&langs_count=6&theme=github_dark&border_color=404040",
-    "codeforces_stats.svg": "https://codeforces-readme-stats.vercel.app/api/card?username=RedHeadphone&theme=github_dark&force_username=true&border_color=404040",
+    "codeforces_stats.svg": "https://codeforces-readme-stats.vercel.app/api/card?username=redheadphone&theme=github_dark&force_username=true&border_color=404040",
     "leetcode_stats.svg": "https://leetcard.jacoblin.cool/redheadphone?theme=dark&font=Ubuntu&cache=14400&ext=contest&sheets=https://gist.githubusercontent.com/RedHeadphone/5e715e284c89cace8f5fa09f7fb930b8/raw/ec0be570f114124b1a2156a660d67baa0ab5639d/leetcode_stats_card.css",
 }
 
